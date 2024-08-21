49e79a9299a6ab7d482ca0ccbc77fe58708640e8
Afsal Baaqir A <afsalbaaqir@gmail.com>
2024-08-21 12:54:00 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (92.65%) - LeetSync

---
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<Integer> luckyNumbers(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        
+        for (int i = 0; i < matrix.length; i++) {
+            int minCol = findMinColumn(matrix, i);
+            int candidate = matrix[i][minCol];
+            
+            if (isMaxInColumn(matrix, candidate, minCol)) {
+                result.add(candidate);
+            }
+        }
+        
+        return result;
+    }
+    
+    private int findMinColumn(int[][] matrix, int row) {
+        int minVal = matrix[row][0], minCol = 0;
+        for (int j = 1; j < matrix[row].length; j++) {
+            if (matrix[row][j] < minVal) {
+                minVal = matrix[row][j];
+                minCol = j;
+            }
+        }
+        return minCol;
+    }
+    
+    private boolean isMaxInColumn(int[][] matrix, int val, int col) {
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][col] > val) return false;
+        }
+        return true;
+    }
+}
