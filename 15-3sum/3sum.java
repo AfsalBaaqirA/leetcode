81066afb95e3649657eb51a4e19e3ec8d0a132e0
Afsal Baaqir A <afsalbaaqir@gmail.com>
2024-08-23 12:36:26 +0530

    Time: 24 ms (97.97%) | Memory: 51.1 MB (93.62%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -7,6 +7,9 @@ class Solution {
         Arrays.sort(nums);
         
         for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
