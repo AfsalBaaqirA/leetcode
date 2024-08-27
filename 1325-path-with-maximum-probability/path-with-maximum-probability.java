67c0670865d2d85398785ac5d48e60a5dfb108de
Afsal Baaqir A <afsalbaaqir@gmail.com>
2024-08-27 13:02:58 +0530

    Time: 9 ms (100.00%) | Memory: 55.9 MB (92.87%) - LeetSync

---
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+
+        double[] prob = new double[n];
+        Arrays.fill(prob, 0.0);
+        prob[start] = 1.0;  
+
+        for (int i = 0; i < n - 1; i++) {
+            boolean updated = false;
+            for (int j = 0; j < edges.length; j++) {
+                int u = edges[j][0];
+                int v = edges[j][1];
+                double edgeProb = succProb[j];
+
+                if (prob[u] * edgeProb > prob[v]) {
+                    prob[v] = prob[u] * edgeProb;
+                    updated = true;
+                }
+                if (prob[v] * edgeProb > prob[u]) {
+                    prob[u] = prob[v] * edgeProb;
+                    updated = true;
+                }
+            }
+            if (!updated) break;  
+        }
+
+        return prob[end];
+    }
+}
