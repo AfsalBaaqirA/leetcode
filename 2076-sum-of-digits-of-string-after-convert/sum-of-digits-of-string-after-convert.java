15c60c17502e5ba7da8a68b13766e4d5df88dba5
Afsal Baaqir A <afsalbaaqir@gmail.com>
2024-09-03 16:51:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (71.17%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.java
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.java
@@ -1,25 +1,26 @@
 class Solution {
     public int getLucky(String s, int k) {
-        StringBuilder sb = new StringBuilder();
+        int res = 0;
 
         for (int i = 0; i < s.length(); i++) {
             int val = s.charAt(i) - 'a' + 1;
-            sb.append(String.valueOf(val));
+
+            if (val >= 10) {
+                res += val / 10 + val % 10;
+            } else {
+                res += val;
+            }
         }
-        s = sb.toString();
-        int res = 0;
-        for (int i = 0; i < s.length(); i++) {
-            res += s.charAt(i) - '0';
-        }
-        while (k > 1) {
-            String tmp = String.valueOf(res);
+
+        while (--k > 0) {
             int temp = 0;
-            for (int i = 0; i < tmp.length(); i++) {
-                temp += tmp.charAt(i) - '0';
+            while (res > 0) {
+                temp += res % 10;
+                res /= 10;
             }
             res = temp;
-            k--;
         }
+
         return res;
     }
 }
